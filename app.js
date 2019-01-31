7585d2b015871915b2317f1c298536e47540dc62
favour ori <orifavour2000@gmail.com>
2019-01-30 21:45:23 -0600

    getting started

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ let express = require("express");
 let app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h3>Welcome to JWT Auth</h3>");
+  res
+    .status(200)
+    .send(
+      "<br/><center><h1>Welcome to JWT Auth</h1><small>Learning Authentication 1.0</small></center>"
+    );
 });
 
-let PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
